697524a836e53379162eed5b41ee2e3e36982211
Jessy Riordan <jessy.riordan@gmail.com>
2015-07-14 16:47:15 -0400

    user data is now stored once logged in

--- app/scripts/beautyMark/login.js
+++ app/scripts/beautyMark/login.js
@@ -1,17 +1,29 @@
 /* global angular Firebase */
-(function(){
-  'use strict';
-  angular.module('beautyMark')
-  .controller('LoginController', function() {
-    var ref = new Firebase("https://beautymark.firebaseio.com");
-    ref.authWithOAuthPopup("facebook", function(error, authData) {
-      if (error) {
-        console.log("Login Failed!", error);
-      } else {
-        console.log("Authenticated successfully with payload:", authData);
-      }},
-      {
-        remember: "sessionOnly"
-    });
-  });
+(function() {
+    'use strict';
+    angular.module('beautyMark')
+      .controller('LoginController', function() {
+          var newUser = true;
+          var ref = new Firebase("https://beautymark.firebaseio.com");
+          ref.authWithOAuthPopup("facebook", function(error, authData) {
+            if (error) {
+              console.log("Login Failed!", error);
+            } else {
+              console.log("Authenticated successfully with payload:", authData);
+            }
+          }, {
+            remember: "sessionOnly"
+          })
+          ref.onAuth(function(authData) {
+            if (authData && newUser) {
+              ref.child("users").child(authData.uid).set({
+                provider: authData.provider,
+                name: getName(authData)
+              });
+            }
+          });
+          function getName(authData) {
+              return authData.facebook.displayName;
+          };
+      });
 })();
