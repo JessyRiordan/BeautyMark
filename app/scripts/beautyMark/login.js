c238b3bb47d23ca421af64822586dd49a46f7981
Jessy Riordan <jessy.riordan@gmail.com>
2015-07-14 13:54:29 -0400

    added dir for controllers

---
+++ app/scripts/beautyMark/login.js
@@ -0,0 +1,15 @@
+/* global angular Firebase */
+(function(){
+  'use strict';
+  angular.module('beautyMark')
+  .controller('LoginController', function() {
+    var ref = new Firebase("https://beautymark.firebaseio.com");
+    ref.authWithOAuthPopup("facebook", function(error, authData) {
+      if (error) {
+        console.log("Login Failed!", error);
+      } else {
+        console.log("Authenticated successfully with payload:", authData);
+      }
+    });
+  });
+})();
