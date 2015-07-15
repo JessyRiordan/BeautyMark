0f893b5c5e47631c72b38f994c421586e0b2f370
Jessy Riordan <jessy.riordan@gmail.com>
2015-07-15 17:49:56 -0400

    working with scope and firebaseArray to add nails

--- app/scripts/beautyMark/addNails.js
+++ app/scripts/beautyMark/addNails.js
@@ -2,9 +2,20 @@
 (function() {
   'use strict';
   angular.module('beautyMark')
-    .controller('AddNailsController', function($http, FIREBASE_URL) {
+    .controller('AddNailsController', function($scope, $firebaseArray, FIREBASE_URL) {
       var ref = new Firebase(FIREBASE_URL);
-      var user = ref.getAuth();
-      $http.get(FIREBASE_URL + user.uid + '/nails.json')
+      $scope.nails = $firebaseArray(ref);
+      $scope.submitNail = function() {
+        $scope.nails.$add({
+          Brand: $scope.newNailsBrand,
+          Name: $scope.newNailsName,
+          Family: $scope.newNailsCFamily,
+          Tone: $scope.newNailsCTone,
+          Finish: $scope.newNailsCFinish,
+          Rating: $scope.newNailsRating
+        });
+      }
+      // var user = ref.getAuth();
+      // $http.get(FIREBASE_URL + user.uid + '/nails.json')
       });
 })();
