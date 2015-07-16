abf9f20836e80241f3ca7fae1cb4ece88f8f9808
Jessy Riordan <jessy.riordan@gmail.com>
2015-07-15 21:33:04 -0400

    adding nail item to firebase is working inconsistantly... why

--- app/scripts/beautyMark/addNails.js
+++ app/scripts/beautyMark/addNails.js
@@ -3,18 +3,39 @@
   'use strict';
   angular.module('beautyMark')
     .controller('AddNailsController', function($scope, $firebaseArray, FIREBASE_URL) {
-      var ref = new Firebase(FIREBASE_URL);
-      $scope.nails = $firebaseArray(ref);
-      $scope.submitNail = function() {
-        $scope.nails.$add({
-          Brand: $scope.newNailsBrand,
-          Name: $scope.newNailsName,
-          Family: $scope.newNailsCFamily,
-          Tone: $scope.newNailsCTone,
-          Finish: $scope.newNailsCFinish,
-          Rating: $scope.newNailsRating
-        });
+      // $scope.items = NailItem.all;
+      // var self = this;
+      // this.numberOf = 0;
+      // $scope.item  = {
+      //   'Brand': '',
+      //   'Name': '',
+      //   'Family': '',
+      //   'Tone': '',
+      //   'Finish': '',
+      //   'Rating': ''
+      // };
+      //
+      // $scope.submitItem = function() {
+      //   NailItem.create($scope.item).then(function(ref) {
+      //     $location.path('/nails/' + ref.name());
+      //   });
+      // };
+      var items = new Firebase(FIREBASE_URL + 'nails');
+      $scope.item = {
+        'Brand': '',
+        'Name': '',
+        'Family': '',
+        'Tone': '',
+        'Finish': '',
+        'Rating': ''
       }
+      console.log($scope.item);
+      $scope.nails = $firebaseArray(items);
+      console.log($scope.nails);
+      $scope.submitNail = function() {
+        $scope.nails.$add($scope.item);
+
+      };
       // var user = ref.getAuth();
       // $http.get(FIREBASE_URL + user.uid + '/nails.json')
       });
