600aaa6385cef50753bbe44d30ba9f2f7b105356
Jessy Riordan <jessy.riordan@gmail.com>
2015-07-17 13:56:49 -0400

    adding nail to firebase plus switching location now works

--- app/scripts/beautyMark/addNails.js
+++ app/scripts/beautyMark/addNails.js
@@ -1,8 +1,8 @@
-/* global angular Firebase JQuery*/
+/* global angular Firebase */
 (function() {
   'use strict';
   angular.module('beautyMark')
-    .controller('AddNailsController', function(FIREBASE_URL, $firebaseArray) {
+    .controller('AddNailsController', function(FIREBASE_URL, $firebaseArray, $location) {
       var ref = new Firebase(FIREBASE_URL);
       var authData = ref.getAuth();
       var nailsRef = ref.child(authData.uid + "/nails");
@@ -16,7 +16,7 @@
       };
       this.nailProduct = $firebaseArray(nailsRef);
       console.log(nailProduct);
-      this.submitNail = function($firebaseArray) {
+      this.submitNail = function() {
         this.nailProduct.$add({
           brand: this.brand,
           name: this.name,
@@ -25,7 +25,10 @@
           finish: this.finish,
           rating: this.rating
         });
-
+        $location.path('/nails');
+      };
+      this.redirect = function() {
+        return $location.path('/nails');
       };
   });
 })();
