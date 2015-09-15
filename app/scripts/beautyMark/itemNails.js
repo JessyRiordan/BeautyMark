c6dd046ec3e33455cd452d80e68663f13f347e99
Jessy Riordan <jessy.riordan@gmail.com>
2015-09-15 09:42:00 -0400

    commented out unused code

--- app/scripts/beautyMark/itemNails.js
+++ app/scripts/beautyMark/itemNails.js
@@ -1,12 +1,12 @@
-/* global angular Firebase */
-(function() {
-  'use strict';
-  angular.module('beautyMark')
-    .controller('ItemNailsController', function(FIREBASE_URL, $routeParams) {
-      var itemId = $routeParams.itemId;
-      console.log(itemId);
-      var ref = new Firebase(FIREBASE_URL);
-      var user = ref.getAuth();
-      var nailsRef = new Firebase(FIREBASE_URL + user.uid + '/nails/:itemId');
-    });
-})();
+// /* global angular Firebase */
+// (function() {
+//   'use strict';
+//   angular.module('beautyMark')
+//     .controller('ItemNailsController', function(FIREBASE_URL, $routeParams) {
+//       var itemId = $routeParams.itemId;
+//       console.log(itemId);
+//       var ref = new Firebase(FIREBASE_URL);
+//       var user = ref.getAuth();
+//       var nailsRef = new Firebase(FIREBASE_URL + user.uid + '/nails/:itemId');
+//     });
+// })();
